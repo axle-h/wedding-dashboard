53adc3fbe56267a5b020a01d0b91f3b268d5c4fc
Alex Haslehurst <alexander.h@alpharooms.com>
2014-11-24 13:23:31 +0000

    Fixed navbar in IE, updated client model binding

--- Axh.Wedding.Mvc/Infrastructure/Helpers/HtmlHelperExtensions.cs
+++ Axh.Wedding.Mvc/Infrastructure/Helpers/HtmlHelperExtensions.cs
@@ -10,23 +10,19 @@
     using Axh.Wedding.Application.ViewModels.Page;
 
     using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
+    using Newtonsoft.Json.Serialization;
 
     public static class HtmlHelperExtensions
     {
+        private static readonly IContractResolver ContractResolver = new BindClientPropertyContractResolver();
+
         public static MvcHtmlString WriteModelJson(this HtmlHelper helper)
         {
             var model = helper.ViewData.Model;
-            var type = model.GetType();
-            var jsonProperties =
-                type.GetProperties()
-                    .Select(prop => new { Attribute = prop.GetCustomAttribute<BindClientPropertyAttribute>(true), Property = prop })
-                    .Where(x => x.Attribute != null)
-                    .Select(x => new JProperty(x.Attribute.Name ?? x.Property.Name, x.Property.GetValue(model) ?? "undefined"));
 
-            var json = new JObject(jsonProperties);
-
-            var tagBuilder = new TagBuilder("script") { InnerHtml = string.Format("var model = {0};", json.ToString(Formatting.None)) };
+            var serializerSettings = new JsonSerializerSettings { ContractResolver = ContractResolver, Formatting = Formatting.None };
+            var json = JsonConvert.SerializeObject(model, serializerSettings);
+            var tagBuilder = new TagBuilder("script") { InnerHtml = string.Format("var model = {0};", json) };
 
             return new MvcHtmlString(tagBuilder.ToString(TagRenderMode.Normal));
         }
@@ -52,6 +48,27 @@
 
             var addressTag = new TagBuilder("address") { InnerHtml = addressHtml };
             return new MvcHtmlString(addressTag.ToString());
+        }
+
+        private class BindClientPropertyContractResolver : DefaultContractResolver
+        {
+            protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+            {
+                var property = base.CreateProperty(member, memberSerialization);
+
+                var bindAttribute = member.GetCustomAttribute<BindClientPropertyAttribute>(true);
+
+                if (bindAttribute == null)
+                {
+                    property.ShouldSerialize = instance => false;
+                }
+                else
+                {
+                    property.PropertyName = bindAttribute.Name;
+                }
+
+                return property;
+            }
         }
 
     }
--- Axh.Wedding.Mvc/Scripts/Site.js
+++ Axh.Wedding.Mvc/Scripts/Site.js
@@ -6,26 +6,25 @@ jQuery(document).ready(function ($) {
     //primary navigation slide-in effect
     if ($(window).width() > MQL) {
         var headerHeight = $('.navbar-custom').height();
-        $(window).on('scroll', {
-                previousTop: 0
-            },
-            function() {
-                var currentTop = $(window).scrollTop();
-                //check if user is scrolling up
-                if (currentTop < this.previousTop) {
-                    //if scrolling up...
-                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-                        $('.navbar-custom').addClass('is-visible');
-                    } else {
-                        $('.navbar-custom').removeClass('is-visible is-fixed');
-                    }
+        var previousTop = 0;
+
+        $(window).scroll(function() {
+            var currentTop = $(window).scrollTop();
+            //check if user is scrolling up
+            if (currentTop < previousTop) {
+                //if scrolling up...
+                if (currentTop == 0) {
+                    $('.navbar-custom').removeClass('is-visible is-fixed');
                 } else {
-                    //if scrolling down...
-                    $('.navbar-custom').removeClass('is-visible');
-                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                    $('.navbar-custom').addClass('is-visible');
                 }
-                this.previousTop = currentTop;
-            });
+            } else if (currentTop != previousTop) {
+                //if scrolling down...
+                $('.navbar-custom').removeClass('is-visible');
+                if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+            }
+            previousTop = currentTop;
+        });
     }
 
 });
