9ce6ad34adf68f67e853db4359b87a766cb242cb
Alex Haslehurst <alex.haslehurst@gmail.com>
2014-12-05 21:47:31 +0000

    Git was ignoring user service

---
+++ Axh.Core.Services.User/UserService.cs
@@ -0,0 +1,56 @@
+﻿namespace Axh.Core.Services.User
+{
+    using System;
+    using System.Threading.Tasks;
+    using Axh.Core.DomainModels.Accounts;
+    using Axh.Core.Repositories.Wedding.Contracts;
+    using Axh.Core.Services.User.Contracts;
+
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository userRepository;
+
+        private readonly IRoleRepository roleRepository;
+
+        public UserService(IUserRepository userRepository, IRoleRepository roleRepository)
+        {
+            this.userRepository = userRepository;
+            this.roleRepository = roleRepository;
+        }
+
+        public async Task CreateAsync(User user)
+        {
+            await this.userRepository.CreateAsync(user);
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            await this.userRepository.UpdateAsync(user);
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            await this.userRepository.DeleteAsync(user);
+        }
+
+        public async Task<User> FindByIdAsync(Guid userId)
+        {
+            return await this.userRepository.FindByIdAsync(userId);
+        }
+
+        public async Task<User> FindByNameAsync(string userName)
+        {
+            return await this.userRepository.FindByNameAsync(userName);
+        }
+
+        public async Task<Role> FindRoleByNameAsync(string roleName)
+        {
+            return await this.roleRepository.FindByNameAsync(roleName);
+        }
+
+        public async Task<User> FindByEmailAsync(string email)
+        {
+            return await this.userRepository.FindByEmailAsync(email);
+        }
+    }
+}
